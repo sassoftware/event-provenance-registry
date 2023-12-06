8c6f48ecfda3f90a5eb94e60776c9b9fd76fe85a
Brett Smith <bc.smith@sas.com>
2023-12-06 16:15:17 -0500

    feat(wip): add client code

--- pkg/api/graphql/schema/types/json.go
+++ pkg/api/graphql/schema/types/json.go
@@ -6,27 +6,16 @@ import (
 	"gorm.io/datatypes"
 )
 
-// JSON type is a wrapper around the datatypes.JSON type in Go.
-// @property  - The code snippet defines a struct named `JSON` which embeds another struct named
-// `datatypes.JSON`. This means that the `JSON` struct inherits all the fields and methods of the
-// `datatypes.JSON` struct.
 // datatype required to be used by gorm so we can support multiple
 // database types for our JSON blobs
 type JSON struct {
 	datatypes.JSON
 }
 
-// ImplementsGraphQLType function is a method of the `JSON` struct. It is used to determine if
-// the struct implements a specific GraphQL type. In this case, the function checks if the input `name`
-// is equal to "JSON". If it is, the function returns `true`, indicating that the struct implements the
-// GraphQL type "JSON".
 func (JSON) ImplementsGraphQLType(name string) bool {
 	return name == "JSON"
 }
 
-// UnmarshalGraphQL function is a method of the `JSON` struct. It is used to convert a GraphQL
-// input value into a Go value. In this case, the function takes an `input` of type `interface{}` and
-// attempts to scan it into the `JSON` struct using the `Scan` method.
 func (j *JSON) UnmarshalGraphQL(input interface{}) error {
 	return j.Scan(input)
 }
--- pkg/api/status.go
+++ pkg/api/status.go
@@ -11,27 +11,19 @@ import (
 	"github.com/sassoftware/event-provenance-registry/pkg/status"
 )
 
-// CheckLiveness() is a method of the `Server` struct. It
-// returns an `http.HandlerFunc` function that handles the liveness check endpoint.
 func (s *Server) CheckLiveness() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		render.JSON(w, r, `{"alive"":true}`)
 	}
 }
 
-// CheckReadiness() is a method of the `Server` struct. It
-// returns an `http.HandlerFunc` function that handles the readiness check endpoint. When this endpoint
-// is called, it will respond with a JSON object `{"ready": true}` using the `render.JSON` function.
 func (s *Server) CheckReadiness() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		render.JSON(w, r, `{"ready":true}`)
 	}
 }
 
-// CheckStatus() is a method of the `Server` struct. It returns
-// an `http.HandlerFunc` function that handles the status check endpoint. When this endpoint is called,
-// it will respond with a JSON object `{"ready": true}` using the `render.JSON` function.
-func (s *Server) CheckStatus(cfg *config.Config) http.HandlerFunc {
+func (s *Server) CheckStatus(cfg *config.ServerConfig) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		s := status.New(cfg)
 		render.JSON(w, r, s)
