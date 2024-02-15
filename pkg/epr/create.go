f80372ad64eb98c0bc2ff5f7328a1408e1b35108
Christian Provenzano <18606244+caproven@users.noreply.github.com>
2024-02-14 22:04:46 -0500

    feat: basic input validation

--- pkg/epr/create.go
+++ pkg/epr/create.go
@@ -1,8 +1,10 @@
 package epr
 
 import (
+	"errors"
 	"fmt"
 	"log/slog"
+	"strings"
 
 	"github.com/graph-gophers/graphql-go"
 	"github.com/sassoftware/event-provenance-registry/pkg/api/graphql/schema/types"
@@ -24,12 +26,73 @@ type EventInput struct {
 	EventReceiverID graphql.ID `json:"event_receiver_id"`
 }
 
+func (e EventInput) Validate() error {
+	var err error
+
+	if strings.TrimSpace(e.Name) == "" {
+		err = errors.Join(err, eprErrors.InvalidInputError{Msg: "name cannot be blank"})
+	}
+	if strings.TrimSpace(e.Version) == "" {
+		err = errors.Join(err, eprErrors.InvalidInputError{Msg: "version cannot be blank"})
+	}
+	if strings.TrimSpace(e.Release) == "" {
+		err = errors.Join(err, eprErrors.InvalidInputError{Msg: "release cannot be blank"})
+	}
+	if strings.TrimSpace(e.PlatformID) == "" {
+		err = errors.Join(err, eprErrors.InvalidInputError{Msg: "platform id cannot be blank"})
+	}
+	if strings.TrimSpace(e.Package) == "" {
+		err = errors.Join(err, eprErrors.InvalidInputError{Msg: "package cannot be blank"})
+	}
+	if strings.TrimSpace(e.Description) == "" {
+		err = errors.Join(err, eprErrors.InvalidInputError{Msg: "description cannot be blank"})
+	}
+	if strings.TrimSpace(string(e.EventReceiverID)) == "" {
+		err = errors.Join(err, eprErrors.InvalidInputError{Msg: "event receiver id cannot be blank"})
+	}
+
+	return err
+}
+
 type EventReceiverInput struct {
 	Name        string     `json:"name"`
 	Type        string     `json:"type"`
 	Version     string     `json:"version"`
 	Description string     `json:"description"`
 	Schema      types.JSON `json:"schema"`
+}
+
+func (r EventReceiverInput) Validate() error {
+	var err error
+
+	if strings.TrimSpace(r.Name) == "" {
+		err = errors.Join(err, eprErrors.InvalidInputError{Msg: "name cannot be blank"})
+	}
+	if strings.TrimSpace(r.Type) == "" {
+		err = errors.Join(err, eprErrors.InvalidInputError{Msg: "type cannot be blank"})
+	}
+	if strings.TrimSpace(r.Version) == "" {
+		err = errors.Join(err, eprErrors.InvalidInputError{Msg: "version cannot be blank"})
+	}
+	if strings.TrimSpace(r.Description) == "" {
+		err = errors.Join(err, eprErrors.InvalidInputError{Msg: "description cannot be blank"})
+	}
+	schemaErr := func() error {
+		schema := r.Schema.String()
+		if schema == "" {
+			return eprErrors.InvalidInputError{Msg: "schema is required"}
+		}
+		loader := gojsonschema.NewStringLoader(schema)
+		_, err := gojsonschema.NewSchema(loader)
+		if err != nil {
+			err = fmt.Errorf("failed to parse schema: %w", err)
+			return eprErrors.InvalidInputError{Msg: err.Error()}
+		}
+		return nil
+	}()
+	err = errors.Join(err, schemaErr)
+
+	return err
 }
 
 type EventReceiverGroupInput struct {
@@ -41,7 +104,43 @@ type EventReceiverGroupInput struct {
 	EventReceiverIDs []graphql.ID `json:"event_receiver_ids"`
 }
 
+func (g EventReceiverGroupInput) Validate() error {
+	var err error
+
+	if strings.TrimSpace(g.Name) == "" {
+		err = errors.Join(err, eprErrors.InvalidInputError{Msg: "name cannot be blank"})
+	}
+	if strings.TrimSpace(g.Type) == "" {
+		err = errors.Join(err, eprErrors.InvalidInputError{Msg: "type cannot be blank"})
+	}
+	if strings.TrimSpace(g.Version) == "" {
+		err = errors.Join(err, eprErrors.InvalidInputError{Msg: "version cannot be blank"})
+	}
+	if strings.TrimSpace(g.Description) == "" {
+		err = errors.Join(err, eprErrors.InvalidInputError{Msg: "description cannot be blank"})
+	}
+	receiverIDErr := func() error {
+		if len(g.EventReceiverIDs) == 0 {
+			return eprErrors.InvalidInputError{Msg: "need at least one event receiver id"}
+		}
+		for _, receiverID := range g.EventReceiverIDs {
+			if strings.TrimSpace(string(receiverID)) == "" {
+				return eprErrors.InvalidInputError{Msg: "event receiver ids cannot be blank"}
+			}
+		}
+		return nil
+	}()
+	err = errors.Join(err, receiverIDErr)
+
+	return err
+}
+
 func CreateEvent(msgProducer message.TopicProducer, db *storage.Database, input EventInput) (*storage.Event, error) {
+	err := input.Validate()
+	if err != nil {
+		return nil, err
+	}
+
 	partial := storage.Event{
 		Name:            input.Name,
 		Version:         input.Version,
@@ -76,15 +175,9 @@ func CreateEvent(msgProducer message.TopicProducer, db *storage.Database, input 
 }
 
 func CreateEventReceiver(msgProducer message.TopicProducer, db *storage.Database, input EventReceiverInput) (*storage.EventReceiver, error) {
-	schema := input.Schema.String()
-	if schema == "" {
-		return nil, eprErrors.InvalidInputError{Msg: "schema is required"}
-	}
-	loader := gojsonschema.NewStringLoader(schema)
-	_, err := gojsonschema.NewSchema(loader)
+	err := input.Validate()
 	if err != nil {
-		err = fmt.Errorf("failed to parse schema: %w", err)
-		return nil, eprErrors.InvalidInputError{Msg: err.Error()}
+		return nil, err
 	}
 
 	partial := storage.EventReceiver{
@@ -108,6 +201,11 @@ func CreateEventReceiver(msgProducer message.TopicProducer, db *storage.Database
 }
 
 func CreateEventReceiverGroup(msgProducer message.TopicProducer, db *storage.Database, input EventReceiverGroupInput) (*storage.EventReceiverGroup, error) {
+	err := input.Validate()
+	if err != nil {
+		return nil, err
+	}
+
 	partial := storage.EventReceiverGroup{
 		Name:             input.Name,
 		Type:             input.Type,
